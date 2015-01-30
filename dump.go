44ab9fec1e8c08ae8b1a446026430a51771a10da
liudng <liudng@gmail.com>
2015-01-30 22:15:53 +0800

    Update repo.

--- dump.go
+++ dump.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The fav Authors. All rights reserved.
+// Copyright 2014 The zhgo Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
