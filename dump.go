87446de19a67d83c74515cf623d2d326b3296716
liudng <liudng@gmail.com>
2015-07-08 17:15:00 +0800

    Comments.

--- dump.go
+++ dump.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The zhgo Authors. All rights reserved.
+// Copyright 2014 The godump Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -11,10 +11,10 @@ import (
 )
 
 type variable struct {
-	//
+	// Output dump string
 	Out string
 
-	//
+	// Indent counter
 	indent int64
 }
 
